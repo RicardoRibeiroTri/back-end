3a47266c6bdd0f35fc3f25d0c69ca19b43f8a042
RicRibeiroTri <ricardoribeirotrindade273@gmail.com>
2025-09-26 11:07:06 -0300

    Add files via upload

---
+++ sistema-de-livros/sistema-de-livros/untitled/src/Aluno.java
@@ -0,0 +1,11 @@
+public class Aluno extends Usuario {
+
+    public Aluno(int id, String nome, String email) {
+        super(id, nome, email);
+    }
+
+    @Override
+    public int getLimiteEmprestimos() {
+        return 2;
+    }
+}
